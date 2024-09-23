0ae7c06be7ca499f73095e9dc75c1bc06d841262
esteban <wpaez7073@gmail.com>
2024-09-23 17:03:21 -0500

    Descripción de los cambios

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),  # URL para la lista de publicaciones
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),  # URL para los detalles de la publicación
 ]
