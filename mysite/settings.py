bcd0b867c9b4612d8959644b461df9a23f757381
esteban <wpaez7073@gmail.com>
2024-09-23 12:12:16 -0500

    Descripción de los cambios

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'aliascosta.pythonanywhere.com'
+    'aliascosta.pythonanywhere.com',
     'www.aliascosta.pythonanywhere.com',
 ]
 
